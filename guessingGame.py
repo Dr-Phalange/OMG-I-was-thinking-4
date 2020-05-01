54e544922301345ee0a89698a6ef1447481127c8
Agidza <50583599+Dr-Phalange@users.noreply.github.com>
2020-05-02 00:53:50 +0300

    Create guessingGame.py

---
+++ guessingGame.py
@@ -0,0 +1,8 @@
+import random
+
+print("What it do baaaabyyyyyy!!!!")
+print("Hi, what is your name?")
+name = input()
+print("Okay " + name + " we are going to play a little game. Have you watched Friends?")
+answer = input()
+if answer 
